b735c08a5b04ceca01ece184f4d162c42d01f90b
Mahdi Lamb <mahdilamb@gmail.com>
2023-09-09 23:06:55 +0100

    add inline assignment

--- dynamic_markdown/utils.py
+++ dynamic_markdown/utils.py
@@ -1,13 +1,18 @@
 """Generate a readme with content derived from python."""
 import ast
+import builtins
 import importlib
 import logging
 import re
+from types import MappingProxyType
 from typing import Any, Dict, Iterable, List, Mapping, Optional, Tuple
 
 logger = logging.getLogger()
+MODULE = re.compile(r"((?:[A-z_][A-z0-9_.]*)|\$)::(.*?)\s*(?:\((.*?)\))", re.M)
 BLOCK = re.compile(
-    r"<!-{2,3}\s*((?:[A-z_][A-z0-9_.]*)|\$)::(.*?)\s*(?:\((.*?)\))(?::(\S*))?\s*-{2,3}>([\S\s]*?)<!-{2,3}\s*\$::end\s*-{2,3}>",
+    r"<!-{2,3}\s*(?:(?:"
+    + MODULE.pattern
+    + r"(?::(\S*))?)|(?:([A-z_][A-z0-9_]*)\s*=\s*([\s\S]*?)))\s*-{2,3}>([\S\s]*?)<!-{2,3}\s*\$::end\s*-{2,3}>",
     re.M,
 )
 GLOBAL_VARIABLE = re.compile(r"(\$[A-z_][A-z0-9_.]*\s*,?)")
@@ -51,6 +56,7 @@ def _environment(values: Optional[Dict[str, Any]] = None):
     """Create an environment."""
     if values is None:
         values = {}
+
     return type(
         "environment", (), {"set": values.__setitem__, "get": values.__getitem__}
     )
@@ -73,20 +79,44 @@ def inject(contents: str) -> str:
     replacements: List[Tuple[int, int, str]] = []
 
     for to_replace in BLOCK.finditer(contents):
-        module_path, fn, parameters, format_spec, _ = to_replace.groups()
-        if module_path == "$":
-            module = env
-        else:
-            module = importlib.import_module(module_path)
-        args, kwargs = convert_parameters(parameters, env)
-        result = getattr(module, fn)(*args, **kwargs)
-        if not result:
-            continue
-        if format_spec:
-            result = format(result, format_spec)
-        else:
-            result = str(result)
-        replacements.append((to_replace.start(5), to_replace.end(5), result))
+        (
+            module_path,
+            fn,
+            parameters,
+            format_spec,
+            assignment,
+            assignee,
+            _,
+        ) = to_replace.groups()
+
+        if assignee:
+            try:
+                env.set(assignment, ast.literal_eval(assignee))
+            except SyntaxError as e:
+                try:
+                    module_path, fn, parameters = next(
+                        MODULE.finditer(assignee)
+                    ).groups()
+                    assignee = None
+                except StopIteration as ee:
+                    raise ee from e
+        if not assignee:
+            if module_path == "$":
+                module = env
+            elif module_path:
+                module = importlib.import_module(module_path)
+            args, kwargs = convert_parameters(parameters, env)
+            result = getattr(module, fn)(*args, **kwargs)
+            if assignment:
+                env.set(assignment, result)
+                result = None
+            if result is None:
+                continue
+            if format_spec:
+                result = format(result, format_spec)
+            else:
+                result = str(result)
+            replacements.append((to_replace.start(7), to_replace.end(7), result))
     for start, end, replacement in replacements[::-1]:
         contents = contents[:start] + replacement + contents[end:]
 
